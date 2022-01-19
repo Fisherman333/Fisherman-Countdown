e604e56f5910d4f2adaa65b278ade5e7afa30a06
Fisherman333 <98053597+Fisherman333@users.noreply.github.com>
2022-01-19 19:27:19 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Unix timestamp (in seconds) to count down to
+    var seeSwey = new Date('2022.01.26 16:00:00').getTime() / 1000
+
+    // Set up FlipDown
+    var flipdown = new FlipDown(seeSwey)
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+        console.log('The countdown has ended!');
+    });
+
+    {
+        theme: "light";
+    }
+    // Show version number
+    var ver = document.getElementById('ver');
+    ver.innerHTML = flipdown.version;
+});
