ea32e4c85a0f2d8a0be378966d31ad853cf6d39f
Fisherman333 <98053597+Fisherman333@users.noreply.github.com>
2022-02-02 00:15:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Unix timestamp (in seconds) to count down to
-    var seeSwey = new Date('2022.01.26 16:00:00').getTime() / 1000
+    var seeSwey = new Date('2022.02.11 16:00:00').getTime() / 1000
 
     // Set up FlipDown
     var flipdown = new FlipDown(seeSwey)
